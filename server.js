f6fbe0f245c44c2a005f0182a43587deb5037efe
RishDevil <rishabhchaubey13@gmail.com>
2023-01-21 09:39:19 +0530

    save

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 try {
   mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
   });
 
